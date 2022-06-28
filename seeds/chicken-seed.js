1d7d4145d03c216b40ed77687ee59d978e36ed13
Magdalena Perry <magperry16@gmail.com>
2022-06-28 11:00:45 -0400

    add carniceria items and run seed

--- seeds/chicken-seed.js
+++ seeds/chicken-seed.js
@@ -6,54 +6,54 @@ const chickenData = [{
   name: "milanesa",
   translated: "grilled steak"
 },
-  {
-    name: "pechuga entera",
-    translated: ""
-  },
-  {
-    name: "pierna",
-    translated: ""
-  },
-  {
-    name: "muslo",
-    translated: "chicken thighs"
-  },
-  {
-    name: "alitas",
-    translated: "",
-  },
-  {
-    name: "alitas preparadas",
-    translated: "",
-  },
-  {
-    name: "pata",
-    translated: ""
-  },
-  {
-    name: "huilotas",
-    translated: ""
-  },
-  {
-    name: "pollo a la naranja",
-    translated: ""
-  },
-  {
-    name: "molleja",
-    translated: ""
-  },
-  {
-    name: "bistec",
-    translated: "",
-  },
-  {
-    name: "entero",
-    translated: "",
-  },
-  {
-    name: "molido",
-    translated: ""
-  },
+{
+  name: "pechuga entera",
+  translated: ""
+},
+{
+  name: "pierna",
+  translated: ""
+},
+{
+  name: "muslo",
+  translated: "chicken thighs"
+},
+{
+  name: "alitas",
+  translated: "",
+},
+{
+  name: "alitas preparadas",
+  translated: "",
+},
+{
+  name: "pata",
+  translated: ""
+},
+{
+  name: "huilotas",
+  translated: ""
+},
+{
+  name: "pollo a la naranja",
+  translated: ""
+},
+{
+  name: "molleja",
+  translated: ""
+},
+{
+  name: "bistec",
+  translated: "",
+},
+{
+  name: "entero",
+  translated: "",
+},
+{
+  name: "molido",
+  translated: ""
+},
 
 
 
